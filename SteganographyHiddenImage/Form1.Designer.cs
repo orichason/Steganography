eef8fe84516e9f5a19407d799c95e33f45ba1014
ori.chason <ori.chason@GMRDL12>
2022-06-23 14:04:58 -0700

    Add project files.

---
+++ SteganographyHiddenImage/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace SteganographyHiddenImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.input1 = new System.Windows.Forms.PictureBox();
+            this.input2 = new System.Windows.Forms.PictureBox();
+            this.output = new System.Windows.Forms.PictureBox();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.decryptImage1 = new System.Windows.Forms.PictureBox();
+            this.decryptImage2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.input1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.input2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.output)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.decryptImage1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.decryptImage2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileDialog
+            // 
+            this.fileDialog.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // input1
+            // 
+            this.input1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.input1.Location = new System.Drawing.Point(12, 65);
+            this.input1.Name = "input1";
+            this.input1.Size = new System.Drawing.Size(250, 250);
+            this.input1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.input1.TabIndex = 1;
+            this.input1.TabStop = false;
+            // 
+            // input2
+            // 
+            this.input2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.input2.Location = new System.Drawing.Point(289, 65);
+            this.input2.Name = "input2";
+            this.input2.Size = new System.Drawing.Size(250, 250);
+            this.input2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.input2.TabIndex = 2;
+            this.input2.TabStop = false;
+            // 
+            // output
+            // 
+            this.output.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.output.Location = new System.Drawing.Point(681, 65);
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(250, 250);
+            this.output.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.output.TabIndex = 3;
+            this.output.TabStop = false;
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(681, 13);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(95, 46);
+            this.EncryptButton.TabIndex = 4;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(545, 343);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(96, 46);
+            this.DecryptButton.TabIndex = 5;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // decryptImage1
+            // 
+            this.decryptImage1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.decryptImage1.Location = new System.Drawing.Point(12, 343);
+            this.decryptImage1.Name = "decryptImage1";
+            this.decryptImage1.Size = new System.Drawing.Size(250, 250);
+            this.decryptImage1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.decryptImage1.TabIndex = 6;
+            this.decryptImage1.TabStop = false;
+            // 
+            // decryptImage2
+            // 
+            this.decryptImage2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.decryptImage2.Location = new System.Drawing.Point(289, 343);
+            this.decryptImage2.Name = "decryptImage2";
+            this.decryptImage2.Size = new System.Drawing.Size(250, 250);
+            this.decryptImage2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.decryptImage2.TabIndex = 7;
+            this.decryptImage2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1242, 653);
+            this.Controls.Add(this.decryptImage2);
+            this.Controls.Add(this.decryptImage1);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.input2);
+            this.Controls.Add(this.input1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.input1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.input2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.output)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.decryptImage1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.decryptImage2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog fileDialog;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox input1;
+        private System.Windows.Forms.PictureBox input2;
+        private System.Windows.Forms.PictureBox output;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.PictureBox decryptImage1;
+        private System.Windows.Forms.PictureBox decryptImage2;
+    }
+}
+
---
+++ SteganographyHiddenImage/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteganographyHiddenImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            WindowState = FormWindowState.Maximized;
+        }
+
+        int count = 0;
+        Bitmap input1Image;
+        Bitmap input2Image;
+
+
+        bool twoImagesSelected = false;
+        void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            DialogResult result = file.ShowDialog();
+            if (result == DialogResult.Cancel || result == DialogResult.No || result == DialogResult.Abort)
+            {
+                MessageBox.Show("Please select an image!");
+                return;
+            }
+
+            string extension = Path.GetExtension(file.FileName);
+
+            if (extension != ".png" && extension != ".jpeg" && extension != ".jfif" && extension != ".bmp")
+            {
+                MessageBox.Show("Please use png, jpeg, or bmp");
+                return;
+
+            }
+
+
+            Bitmap loadedImage = new Bitmap(file.FileName);
+
+            if (count % 2 == 0)
+            {
+                input1Image = loadedImage;
+                input1.Image = loadedImage;
+                //input2.Location = new Point(input1.Right, input1.Top);
+                count++;
+            }
+            else
+            {
+                if (loadedImage.Size == input1Image.Size)
+                {
+                    input2Image = loadedImage;
+                    input2.Image = loadedImage;
+                    count++;
+                    twoImagesSelected = true;
+                }
+
+                else
+                {
+                    MessageBox.Show("Error: please match size with other image");
+                }
+
+                //Make sure we are the same size as image1
+            }
+
+
+
+        }
+
+        string FormatBinaryString(StringBuilder binaryString, int paddedSize = 4)
+        {
+            int roundUp = paddedSize - binaryString.Length % paddedSize;
+            if (roundUp == paddedSize)
+            {
+                roundUp = 0;
+            }
+
+            for (int i = 0; i < roundUp; i++)
+            {
+                binaryString.Insert(0, '0');
+            }
+
+            return binaryString.ToString();
+        }
+        string DecimaltoBinary(int number)
+        {
+            StringBuilder binaryString = new StringBuilder();
+
+            //int number = int.Parse(decimalStr);
+
+            do
+            {
+                int remainder = number % 2;
+
+                number /= 2;
+
+                binaryString.Insert(0, remainder);
+
+            } while (number > 0);
+
+            return FormatBinaryString(binaryString);
+        }
+
+        int BinaryToDecimal(string binaryStr)
+        {
+            int finalDecimal = 0;
+            for (int i = binaryStr.Length - 1; i >= 0; i--)
+            {
+                int digit = binaryStr[i] - '0';
+                int newDecimal = digit * (int)Math.Pow(2, binaryStr.Length - (i + 1));
+                finalDecimal += newDecimal;
+            }
+
+            return finalDecimal;
+        }
+
+        byte GetSigFigs(byte byte1, byte byte2)
+        {
+            string byte1inBinary = DecimaltoBinary(byte1);
+            string byte2inBinary = DecimaltoBinary(byte2);
+
+            byte1inBinary = byte1inBinary.PadLeft(8, '0');
+            byte2inBinary = byte2inBinary.PadLeft(8, '0');
+
+            string result = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                result += byte1inBinary[i];
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                result += byte2inBinary[i];
+            }
+
+            byte newByte = (byte)BinaryToDecimal(result);
+
+            return newByte;
+        }
+
+        private Color EncryptColor(Color a, Color b)
+        {
+            byte newR = GetSigFigs(a.R, b.R);
+            byte newG = GetSigFigs(a.G, b.G);
+            byte newB = GetSigFigs(a.B, b.B);
+
+            Color newColor = Color.FromArgb(newR, newG, newB);
+
+            return newColor;
+        }
+
+        Bitmap newImage;
+        private void EncryptButton_Click(object sender, EventArgs e)
+        {
+            //1.) Create image of correct size (size of the input images)
+            //2.) Loop over every pixel in that new image
+            //3.) Set each pixel to be yellow <-> Replace with taking most/least sig figs and swapping them
+            //4.) Display image
+
+            newImage = new Bitmap(input1Image.Width, input1Image.Height);
+
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    //Store R, G, B values for each input image
+                    Color input1Color = input1Image.GetPixel(x, y);
+                    Color input2Color = input2Image.GetPixel(x, y);
+                    Color encrypt = EncryptColor(input1Color, input2Color);
+                    newImage.SetPixel(x, y, encrypt);
+                }
+            }
+
+            output.Image = newImage;
+        }
+
+        (byte, byte) SplittingByte(byte byte1)
+        {
+            //Get byte from pixel
+            //split first half of byte and add '0000' to the end
+            //take second half and add '0000' to the end
+            //image1: first half
+            //image2: second half
+
+            string byteInBinary = DecimaltoBinary(byte1);
+            byteInBinary = byteInBinary.PadLeft(8, '0');
+
+            string image1ResultInString = byteInBinary.Substring(0, 4);
+            //*******************make sure byte1InBinary is padded
+            string image2ResultInString = byteInBinary.Substring(4, 4);
+
+            image1ResultInString += "0000";
+            image2ResultInString += "0000";
+
+            int image1ResultInt = BinaryToDecimal(image1ResultInString);
+            int image2ResultInt = BinaryToDecimal(image2ResultInString);
+
+            return ((byte)image1ResultInt, (byte)image2ResultInt);
+        }
+
+        private void DecryptButton_Click(object sender, EventArgs e)
+        {
+            Bitmap DecryptImage1 = new Bitmap(newImage.Width, newImage.Height);
+            Bitmap DecryptImage2 = new Bitmap(newImage.Width, newImage.Height);
+            //(byte a, byte b) = Decrypt(43);
+            for (int x = 0; x < newImage.Width; x++)
+            {
+                for (int y = 0; y < newImage.Height; y++)
+                {
+                    Color currentPositionColor = newImage.GetPixel(x, y);
+
+                    (byte r1, byte r2) = SplittingByte(currentPositionColor.R);
+                    (byte g1, byte g2) = SplittingByte(currentPositionColor.G);
+                    (byte b1, byte b2) = SplittingByte(currentPositionColor.B);
+
+                    DecryptImage1.SetPixel(x, y, Color.FromArgb(r1, g1, b1));
+                    DecryptImage2.SetPixel(x, y, Color.FromArgb(r2, g2, b2));
+                }
+                
+            }
+
+            decryptImage1.Image = DecryptImage1;
+            decryptImage2.Image = DecryptImage2;
+        }
+    }
+}
